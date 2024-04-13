a5befe037c14b300c79df327a39a7d78471f0f1c
Chimdinma Obasi <166870670+codebydinma@users.noreply.github.com>
2024-04-13 09:46:51 +0100

    Update main.py
    Connect with HTML elements to pull data

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
 import pandas as pd
 
 def map_details():
@@ -17,14 +21,14 @@ def map_details():
     reviews: str =  []
 
     df = pd.DataFrame({"name" : name,
-        "phone" : phone,
-        "address" : address,
-        "longitude" : longitude,
-        "latitude" : latitude,
-        "website" : website,
+        "phone": phone,
+        "address": address,
+        "longitude": longitude,
+        "latitude": latitude,
+        "website": website,
         "open_time" : open_time,
         "close_time" : close_time,
-        "reviews" : reviews})
+        "reviews": reviews})
 
     return df
 
@@ -37,5 +41,18 @@ search_info = input("Enter your search: ")
 
 elem.send_keys(search_info)
 elem.send_keys(Keys.RETURN)
+
+wait = WebDriverWait(driver, 40)
+wait.until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@class,'m6QErb')]")))
+
+items = []
+
+# Wait for links and extract
+results = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//div[contains(@class,'Nv2PK')]")))
+for result in results:
+    element = result.find_element("XPath","//div[contains(@class,'NrDZNb')]")
+    link = element.text
+    items.append(link)
+    print(items)
 
 driver.close()
